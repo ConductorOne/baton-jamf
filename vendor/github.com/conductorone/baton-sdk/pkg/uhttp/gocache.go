bb9ec66e4d3230042346d78df28bcbc30ac59f00
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:24:26 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.x
    - Update baton-sdk dependency to v0.7.4
    - Update Go version to 1.25.2
    - Update field.Configuration API
    - Add WithDefaultCapabilitiesConnectorBuilder option
    - Update CI workflow to use go-version-file
    - Update golangci-lint-action to v8
    - Update .golangci.yml to v2 format
    - Add tools/rules.go for ruleguard

--- vendor/github.com/conductorone/baton-sdk/pkg/uhttp/gocache.go
+++ vendor/github.com/conductorone/baton-sdk/pkg/uhttp/gocache.go
@@ -4,32 +4,24 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
+	"math"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"strconv"
 	"time"
 
-	bigCache "github.com/allegro/bigcache/v3"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"github.com/maypok86/otter/v2"
+	"github.com/maypok86/otter/v2/stats"
 	"go.uber.org/zap"
 )
 
 const (
-	cacheTTLMaximum  = 31536000 // 31536000 seconds = one year
-	cacheTTLDefault  = 3600     // 3600 seconds = one hour
-	defaultCacheSize = 5        // MB
-)
-
-type CacheBehavior string
-
-const (
-	CacheBehaviorDefault CacheBehavior = "default"
-	// On the first time setting a value, make it empty. Get will return not found, so uhttp Do() will set it again.
-	// Then actually set the value. This effectively makes the cache only cache things that have been requested more than once.
-	CacheBehaviorSparse CacheBehavior = "sparse"
+	cacheTTLMaximum    time.Duration = 31536000 * time.Second // 31536000 seconds = one year
+	cacheTTLDefault    time.Duration = 3600 * time.Second     // 3600 seconds = one hour
+	defaultCacheSizeMb uint64        = 5                      // MB
 )
 
 type CacheBackend string
@@ -41,27 +33,25 @@ const (
 )
 
 type CacheConfig struct {
-	LogDebug bool
-	TTL      int64 // If 0, cache is disabled
-	MaxSize  int   // MB
-	Behavior CacheBehavior
-	Backend  CacheBackend
+	LogDebug  bool
+	TTL       time.Duration // If 0, cache is disabled
+	MaxSizeMb uint64        // MB
+	Backend   CacheBackend  // If noop, cache is disabled
 }
 
 type CacheStats struct {
-	Hits   int64
-	Misses int64
+	Hits   uint64
+	Misses uint64
 }
 
 type ContextKey struct{}
 
 type GoCache struct {
-	rootLibrary *bigCache.BigCache
-	behavior    CacheBehavior
+	rootLibrary *otter.Cache[string, []byte]
 }
 
 type NoopCache struct {
-	counter int64
+	counter uint64
 }
 
 func NewNoopCache(ctx context.Context) *NoopCache {
@@ -90,16 +80,15 @@ func (n *NoopCache) Stats(ctx context.Context) CacheStats {
 }
 
 func (cc *CacheConfig) ToString() string {
-	return fmt.Sprintf("Backend: %v, TTL: %d, MaxSize: %dMB, LogDebug: %t, Behavior: %v", cc.Backend, cc.TTL, cc.MaxSize, cc.LogDebug, cc.Behavior)
+	return fmt.Sprintf("Backend: %v, TTL: %d, MaxSize: %dMB, LogDebug: %t", cc.Backend, cc.TTL, cc.MaxSizeMb, cc.LogDebug)
 }
 
 func DefaultCacheConfig() CacheConfig {
 	return CacheConfig{
-		TTL:      cacheTTLDefault,
-		MaxSize:  defaultCacheSize,
-		LogDebug: false,
-		Behavior: CacheBehaviorDefault,
-		Backend:  CacheBackendMemory,
+		TTL:       cacheTTLDefault,
+		MaxSizeMb: defaultCacheSizeMb,
+		LogDebug:  false,
+		Backend:   CacheBackendMemory,
 	}
 }
 
@@ -107,17 +96,13 @@ func NewCacheConfigFromEnv() *CacheConfig {
 	config := DefaultCacheConfig()
 
 	cacheMaxSize, err := strconv.ParseInt(os.Getenv("BATON_HTTP_CACHE_MAX_SIZE"), 10, 64)
-	if err == nil {
-		config.MaxSize = int(cacheMaxSize)
+	if err == nil && cacheMaxSize >= 0 {
+		config.MaxSizeMb = uint64(cacheMaxSize)
 	}
 
-	// read the `BATON_HTTP_CACHE_TTL` environment variable and return
-	// the value as a number of seconds between 0 and an arbitrary maximum. Note:
-	// this means that passing a value of `-1` will set the TTL to zero rather than
-	// infinity.
 	cacheTTL, err := strconv.ParseInt(os.Getenv("BATON_HTTP_CACHE_TTL"), 10, 64)
 	if err == nil {
-		config.TTL = min(cacheTTLMaximum, max(0, cacheTTL))
+		config.TTL = min(cacheTTLMaximum, max(0, time.Duration(cacheTTL)*time.Second))
 	}
 
 	cacheBackend := os.Getenv("BATON_HTTP_CACHE_BACKEND")
@@ -128,14 +113,6 @@ func NewCacheConfigFromEnv() *CacheConfig {
 		config.Backend = CacheBackendMemory
 	case "noop":
 		config.Backend = CacheBackendNoop
-	}
-
-	cacheBehavior := os.Getenv("BATON_HTTP_CACHE_BEHAVIOR")
-	switch cacheBehavior {
-	case "sparse":
-		config.Behavior = CacheBehaviorSparse
-	case "default":
-		config.Behavior = CacheBehaviorDefault
 	}
 
 	disableCache, err := strconv.ParseBool(os.Getenv("BATON_DISABLE_HTTP_CACHE"))
@@ -170,8 +147,8 @@ func NewHttpCache(ctx context.Context, config *CacheConfig) (icache, error) {
 
 	l.Info("http cache config", zap.String("config", config.ToString()))
 
-	if config.TTL <= 0 {
-		l.Debug("CacheTTL is <=0, disabling cache.", zap.Int64("CacheTTL", config.TTL))
+	if config.TTL == 0 {
+		l.Debug("NewHttpCache: Cache TTL is 0, disabling cache.", zap.Duration("cache_ttl", config.TTL))
 		return NewNoopCache(ctx), nil
 	}
 
@@ -203,28 +180,29 @@ func NewHttpCache(ctx context.Context, config *CacheConfig) (icache, error) {
 func NewGoCache(ctx context.Context, cfg CacheConfig) (*GoCache, error) {
 	l := ctxzap.Extract(ctx)
 	gc := GoCache{}
-	gc.behavior = cfg.Behavior
-	config := bigCache.DefaultConfig(time.Duration(cfg.TTL) * time.Second)
-	config.Verbose = cfg.LogDebug
-	config.Shards = 4
-	config.HardMaxCacheSize = cfg.MaxSize // value in MB, 0 value means no size limit
-	cache, err := bigCache.New(ctx, config)
+	maxSize := cfg.MaxSizeMb * 1024 * 1024
+	if maxSize > math.MaxInt {
+		return nil, fmt.Errorf("error converting max size to bytes")
+	}
+	cache, err := otter.New(&otter.Options[string, []byte]{
+		MaximumWeight: maxSize,
+		StatsRecorder: stats.NewCounter(),
+		Weigher: func(key string, value []byte) uint32 {
+			weight64 := uint64(len(key)) + uint64(len(value))
+			if weight64 > uint64(math.MaxUint32) {
+				return math.MaxUint32
+			}
+			return uint32(weight64)
+		},
+		ExpiryCalculator: otter.ExpiryWriting[string, []byte](cfg.TTL),
+	})
+
 	if err != nil {
-		l.Error("bigcache initialization error", zap.Error(err))
+		l.Error("cache initialization error", zap.Error(err))
 		return nil, err
 	}
 
-	l.Debug("bigcache config",
-		zap.Dict("config",
-			zap.Int("Shards", config.Shards),
-			zap.Duration("LifeWindow", config.LifeWindow),
-			zap.Duration("CleanWindow", config.CleanWindow),
-			zap.Int("MaxEntriesInWindow", config.MaxEntriesInWindow),
-			zap.Int("MaxEntrySize", config.MaxEntrySize),
-			zap.Bool("StatsEnabled", config.StatsEnabled),
-			zap.Bool("Verbose", config.Verbose),
-			zap.Int("HardMaxCacheSize", config.HardMaxCacheSize),
-		))
+	l.Debug("otter cache initialized", zap.Uint64("capacity", cache.GetMaximum()))
 	gc.rootLibrary = cache
 
 	return &gc, nil
@@ -251,12 +229,9 @@ func (g *GoCache) Get(req *http.Request) (*http.Response, error) {
 		return nil, err
 	}
 
-	entry, err := g.rootLibrary.Get(key)
-	if err != nil {
-		if errors.Is(err, bigCache.ErrEntryNotFound) {
-			return nil, nil
-		}
-		return nil, err
+	entry, found := g.rootLibrary.GetIfPresent(key)
+	if !found {
+		return nil, nil
 	}
 
 	if len(entry) == 0 {
@@ -287,23 +262,9 @@ func (g *GoCache) Set(req *http.Request, value *http.Response) error {
 		return err
 	}
 
-	// If in sparse mode, the first time we call set on a key we make the value empty bytes.
-	// Subsequent calls to set actually set the value.
-	if g.behavior == CacheBehaviorSparse {
-		_, err := g.rootLibrary.Get(key)
-		if err != nil && !errors.Is(err, bigCache.ErrEntryNotFound) {
-			return err
-		}
-
-		if errors.Is(err, bigCache.ErrEntryNotFound) {
-			newValue = []byte{}
-		}
-	}
-
-	err = g.rootLibrary.Set(key, newValue)
-	if err != nil {
-		return err
-	}
+	// Otter's cost function rejects large responses if there's not enough room
+	// TODO: return some error or warning that we couldn't set?
+	_, _ = g.rootLibrary.Set(key, newValue)
 
 	return nil
 }
@@ -313,10 +274,7 @@ func (g *GoCache) Delete(key string) error {
 		return nil
 	}
 
-	err := g.rootLibrary.Delete(key)
-	if err != nil {
-		return err
-	}
+	g.rootLibrary.Invalidate(key)
 
 	return nil
 }
@@ -328,14 +286,7 @@ func (g *GoCache) Clear(ctx context.Context) error {
 		return nil
 	}
 
-	err := g.rootLibrary.Reset()
-	if err != nil {
-		return err
-	}
-	err = g.rootLibrary.ResetStats()
-	if err != nil {
-		return err
-	}
+	g.rootLibrary.InvalidateAll()
 
 	l.Debug("reset cache")
 	return nil
@@ -345,6 +296,6 @@ func (g *GoCache) Has(key string) bool {
 	if g.rootLibrary == nil {
 		return false
 	}
-	_, found := g.rootLibrary.Get(key)
-	return found == nil
+	_, found := g.rootLibrary.GetIfPresent(key)
+	return found
 }
--- vendor/github.com/conductorone/baton-sdk/pkg/uhttp/wrapper.go
+++ vendor/github.com/conductorone/baton-sdk/pkg/uhttp/wrapper.go
@@ -10,6 +10,9 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
+	"syscall"
 	"time"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
@@ -29,9 +32,8 @@ const (
 	applicationFormUrlencoded = "application/x-www-form-urlencoded"
 	applicationVndApiJSON     = "application/vnd.api+json"
 	acceptHeader              = "Accept"
+	authorizationHeader       = "Authorization"
 )
-
-const maxBodySize = 4096
 
 type WrapperResponse struct {
 	Header     http.Header
@@ -132,14 +134,14 @@ func NewBaseHttpClientWithContext(ctx context.Context, httpClient *http.Client, 
 // WithJSONResponse is a wrapper that marshals the returned response body into
 // the provided shape. If the API should return an empty JSON body (i.e. HTTP
 // status code 204 No Content), then pass a `nil` to `response`.
-func WithJSONResponse(response interface{}) DoOption {
+func WithJSONResponse(response any) DoOption {
 	return func(resp *WrapperResponse) error {
 		contentHeader := resp.Header.Get(ContentType)
 
 		if !IsJSONContentType(contentHeader) {
 			if len(resp.Body) != 0 {
-				// we want to see the body regardless
-				return fmt.Errorf("unexpected content type for JSON response: %s. status code: %d. body: «%s»", contentHeader, resp.StatusCode, logBody(resp.Body, maxBodySize))
+				// to print the response, set the envvar BATON_DEBUG_PRINT_RESPONSE_BODY as non-empty, instead
+				return fmt.Errorf("unexpected content type for JSON response: %s. status code: %d", contentHeader, resp.StatusCode)
 			}
 			return fmt.Errorf("unexpected content type for JSON response: %s. status code: %d", contentHeader, resp.StatusCode)
 		}
@@ -148,17 +150,56 @@ func WithJSONResponse(response interface{}) DoOption {
 		}
 		err := json.Unmarshal(resp.Body, response)
 		if err != nil {
-			return fmt.Errorf("failed to unmarshal json response: %w. status code: %d. body %v", err, resp.StatusCode, logBody(resp.Body, maxBodySize))
+			// to print the response, set the envvar BATON_DEBUG_PRINT_RESPONSE_BODY as non-empty, instead
+			return fmt.Errorf("failed to unmarshal json response: %w. status code: %d", err, resp.StatusCode)
 		}
 		return nil
 	}
 }
 
-func logBody(body []byte, size int) string {
-	if len(body) > size {
-		return string(body[:size]) + " ..."
+// Ignore content type header and always try to parse the response as JSON.
+func WithAlwaysJSONResponse(response any) DoOption {
+	return func(resp *WrapperResponse) error {
+		if response == nil && len(resp.Body) == 0 {
+			return nil
+		}
+		err := json.Unmarshal(resp.Body, response)
+		if err != nil {
+			// to print the response, set the envvar BATON_DEBUG_PRINT_RESPONSE_BODY as non-empty, instead
+			return fmt.Errorf("failed to unmarshal json response: %w. status code: %d", err, resp.StatusCode)
+		}
+		return nil
 	}
-	return string(body)
+}
+
+func WithXMLResponse(response any) DoOption {
+	return func(resp *WrapperResponse) error {
+		if !IsXMLContentType(resp.Header.Get(ContentType)) {
+			return fmt.Errorf("unexpected content type for xml response: %s", resp.Header.Get(ContentType))
+		}
+		if response == nil && len(resp.Body) == 0 {
+			return nil
+		}
+		err := xml.Unmarshal(resp.Body, response)
+		if err != nil {
+			return fmt.Errorf("failed to unmarshal xml response: %w. status code: %d", err, resp.StatusCode)
+		}
+		return nil
+	}
+}
+
+// Ignore content type header and always try to parse the response as XML.
+func WithAlwaysXMLResponse(response any) DoOption {
+	return func(resp *WrapperResponse) error {
+		if response == nil && len(resp.Body) == 0 {
+			return nil
+		}
+		err := xml.Unmarshal(resp.Body, response)
+		if err != nil {
+			return fmt.Errorf("failed to unmarshal xml response: %w. status code: %d", err, resp.StatusCode)
+		}
+		return nil
+	}
 }
 
 type ErrorResponse interface {
@@ -174,12 +215,14 @@ func WithErrorResponse(resource ErrorResponse) DoOption {
 		contentHeader := resp.Header.Get(ContentType)
 
 		if !IsJSONContentType(contentHeader) {
-			return fmt.Errorf("unexpected content type for JSON error response: %s. status code: %d. body: «%s»", contentHeader, resp.StatusCode, logBody(resp.Body, maxBodySize))
+			// to print the response, set the envvar BATON_DEBUG_PRINT_RESPONSE_BODY as non-empty, instead
+			return fmt.Errorf("unexpected content type for JSON error response: %s. status code: %d. body: %s", contentHeader, resp.StatusCode, string(resp.Body))
 		}
 
 		// Decode the JSON response body into the ErrorResponse
 		if err := json.Unmarshal(resp.Body, &resource); err != nil {
-			return fmt.Errorf("failed to unmarshal JSON error response: %w. status code: %d. body %v", err, resp.StatusCode, logBody(resp.Body, maxBodySize))
+			// to print the response, set the envvar BATON_DEBUG_PRINT_RESPONSE_BODY as non-empty, instead
+			return fmt.Errorf("failed to unmarshal JSON error response: %w. status code: %d. body: %s", err, resp.StatusCode, string(resp.Body))
 		}
 
 		// Construct a more detailed error message
@@ -191,37 +234,24 @@ func WithErrorResponse(resource ErrorResponse) DoOption {
 
 func WithRatelimitData(resource *v2.RateLimitDescription) DoOption {
 	return func(resp *WrapperResponse) error {
+		if resource == nil {
+			return fmt.Errorf("WithRatelimitData: rate limit description is nil")
+		}
 		rl, err := ratelimit.ExtractRateLimitData(resp.StatusCode, &resp.Header)
 		if err != nil {
 			return err
 		}
 
-		resource.Limit = rl.Limit
-		resource.Remaining = rl.Remaining
-		resource.ResetAt = rl.ResetAt
-		resource.Status = rl.Status
+		resource.SetLimit(rl.GetLimit())
+		resource.SetRemaining(rl.GetRemaining())
+		resource.SetResetAt(rl.GetResetAt())
+		resource.SetStatus(rl.GetStatus())
 
 		return nil
 	}
 }
 
-func WithXMLResponse(response interface{}) DoOption {
-	return func(resp *WrapperResponse) error {
-		if !IsXMLContentType(resp.Header.Get(ContentType)) {
-			return fmt.Errorf("unexpected content type for xml response: %s", resp.Header.Get(ContentType))
-		}
-		if response == nil && len(resp.Body) == 0 {
-			return nil
-		}
-		err := xml.Unmarshal(resp.Body, response)
-		if err != nil {
-			return fmt.Errorf("failed to unmarshal xml response: %w. body %v", err, resp.Body)
-		}
-		return nil
-	}
-}
-
-func WithResponse(response interface{}) DoOption {
+func WithResponse(response any) DoOption {
 	return func(resp *WrapperResponse) error {
 		if IsJSONContentType(resp.Header.Get(ContentType)) {
 			return WithJSONResponse(response)(resp)
@@ -232,6 +262,66 @@ func WithResponse(response interface{}) DoOption {
 
 		return status.Error(codes.Unknown, "unsupported content type")
 	}
+}
+
+// Handle anything that can be marshaled into JSON or XML.
+// If the response is a list, its values will be put into the "items" field.
+func WithGenericResponse(response *map[string]any) DoOption {
+	return func(resp *WrapperResponse) error {
+		if response == nil {
+			return status.Error(codes.InvalidArgument, "response is nil")
+		}
+
+		if resp.StatusCode == http.StatusNoContent {
+			return nil
+		}
+
+		var v any
+		var err error
+
+		if IsJSONContentType(resp.Header.Get(ContentType)) {
+			err = WithJSONResponse(&v)(resp)
+			if err != nil {
+				return err
+			}
+			if list, ok := v.([]any); ok {
+				(*response)["items"] = list
+			} else if vMap, ok := v.(map[string]any); ok {
+				*response = vMap
+			} else {
+				return status.Errorf(codes.Internal, "unsupported content type: %T", v)
+			}
+			return nil
+		}
+
+		if IsXMLContentType(resp.Header.Get(ContentType)) {
+			err = WithXMLResponse(response)(resp)
+			if err != nil {
+				return err
+			}
+			if list, ok := v.([]any); ok {
+				(*response)["items"] = list
+			} else if vMap, ok := v.(map[string]any); ok {
+				*response = vMap
+			} else {
+				return status.Errorf(codes.Internal, "unsupported content type: %T", v)
+			}
+			return nil
+		}
+
+		return status.Error(codes.Unknown, fmt.Sprintf("unsupported content type: %s", resp.Header.Get(ContentType)))
+	}
+}
+
+func WrapErrors(preferredCode codes.Code, statusMsg string, errs ...error) error {
+	st := status.New(preferredCode, statusMsg)
+
+	if len(errs) == 0 {
+		return st.Err()
+	}
+
+	allErrs := append([]error{st.Err()}, errs...)
+	return errors.Join(allErrs...)
 }
 
 func WrapErrorsWithRateLimitInfo(preferredCode codes.Code, resp *http.Response, errs ...error) error {
@@ -263,7 +353,7 @@ func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Respo
 		c.rateLimiter.Take()
 	}
 
-	if req.Method == http.MethodGet {
+	if req.Method == http.MethodGet && req.Header.Get("Cache-Control") != "no-cache" {
 		resp, err = c.baseHttpCache.Get(req)
 		if err != nil {
 			return nil, err
@@ -278,10 +368,21 @@ func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Respo
 	if resp == nil {
 		resp, err = c.HttpClient.Do(req)
 		if err != nil {
+			l.Error("base-http-client: HTTP error response", zap.Error(err))
+			// Turn certain network errors into grpc statuses so we retry
+			if errors.Is(err, io.ErrUnexpectedEOF) {
+				return resp, WrapErrors(codes.Unavailable, "unexpected EOF", err)
+			}
+			if errors.Is(err, syscall.ECONNRESET) {
+				return nil, WrapErrors(codes.Unavailable, "connection reset", err)
+			}
 			var urlErr *url.Error
 			if errors.As(err, &urlErr) {
 				if urlErr.Timeout() {
-					return nil, status.Error(codes.DeadlineExceeded, fmt.Sprintf("request timeout: %v", urlErr.URL))
+					return nil, WrapErrors(codes.DeadlineExceeded, fmt.Sprintf("request timeout: %v", urlErr.URL), urlErr)
+				}
+				if urlErr.Temporary() {
+					return nil, WrapErrors(codes.Unavailable, fmt.Sprintf("temporary error: %v", urlErr.URL), urlErr)
 				}
 			}
 			if errors.Is(err, context.DeadlineExceeded) {
@@ -297,11 +398,23 @@ func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Respo
 		if len(body) > 0 {
 			resp.Body = io.NopCloser(bytes.NewBuffer(body))
 		}
+		// Turn certain body read errors into grpc statuses so we retry
+		if errors.Is(err, io.ErrUnexpectedEOF) {
+			return resp, WrapErrors(codes.Unavailable, "unexpected EOF", err)
+		}
+		if errors.Is(err, syscall.ECONNRESET) {
+			return resp, WrapErrors(codes.Unavailable, "connection reset", err)
+		}
 		return resp, err
 	}
 
 	// Replace resp.Body with a no-op closer so nobody has to worry about closing the reader.
-	resp.Body = io.NopCloser(bytes.NewBuffer(body))
+	shouldPrint := os.Getenv("BATON_DEBUG_PRINT_RESPONSE_BODY")
+	if shouldPrint != "" {
+		resp.Body = io.NopCloser(wrapPrintBody(bytes.NewBuffer(body)))
+	} else {
+		resp.Body = io.NopCloser(bytes.NewBuffer(body))
+	}
 
 	wresp := WrapperResponse{
 		Header:     resp.Header,
@@ -318,10 +431,20 @@ func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Respo
 		}
 	}
 
+	// Log response headers directly for certain errors
+	if resp.StatusCode >= 400 {
+		redactedHeaders := RedactSensitiveHeaders(resp.Header)
+		l.Error("base-http-client: HTTP error status",
+			zap.Int("status_code", resp.StatusCode),
+			zap.String("status", resp.Status),
+			zap.Any("headers", redactedHeaders),
+		)
+	}
+
 	switch resp.StatusCode {
 	case http.StatusRequestTimeout:
 		return resp, WrapErrorsWithRateLimitInfo(codes.DeadlineExceeded, resp, optErrs...)
-	case http.StatusTooManyRequests, http.StatusServiceUnavailable:
+	case http.StatusTooManyRequests, http.StatusBadGateway, http.StatusServiceUnavailable, http.StatusGatewayTimeout:
 		return resp, WrapErrorsWithRateLimitInfo(codes.Unavailable, resp, optErrs...)
 	case http.StatusNotFound:
 		return resp, WrapErrorsWithRateLimitInfo(codes.NotFound, resp, optErrs...)
@@ -329,6 +452,8 @@ func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Respo
 		return resp, WrapErrorsWithRateLimitInfo(codes.Unauthenticated, resp, optErrs...)
 	case http.StatusForbidden:
 		return resp, WrapErrorsWithRateLimitInfo(codes.PermissionDenied, resp, optErrs...)
+	case http.StatusConflict:
+		return resp, WrapErrorsWithRateLimitInfo(codes.AlreadyExists, resp, optErrs...)
 	case http.StatusNotImplemented:
 		return resp, WrapErrorsWithRateLimitInfo(codes.Unimplemented, resp, optErrs...)
 	}
@@ -351,6 +476,40 @@ func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Respo
 	return resp, errors.Join(optErrs...)
 }
 
+var sensitiveStrings = []string{
+	"api-key",
+	"auth",
+	"cookie",
+	"proxy-authorization",
+	"set-cookie",
+	"x-forwarded-for",
+	"x-forwarded-proto",
+}
+
+func RedactSensitiveHeaders(h http.Header) http.Header {
+	if h == nil {
+		return nil
+	}
+	safe := make(http.Header, len(h))
+	for k, v := range h {
+		sensitive := false
+		headerKey := strings.ToLower(k)
+		for _, sensitiveString := range sensitiveStrings {
+			if strings.Contains(headerKey, sensitiveString) {
+				sensitive = true
+				break
+			}
+		}
+
+		if sensitive {
+			safe[k] = []string{"REDACTED"}
+		} else {
+			safe[k] = v
+		}
+	}
+	return safe
+}
+
 func WithHeader(key, value string) RequestOption {
 	return func() (io.ReadWriter, map[string]string, error) {
 		return nil, map[string]string{
@@ -359,7 +518,21 @@ func WithHeader(key, value string) RequestOption {
 	}
 }
 
-func WithJSONBody(body interface{}) RequestOption {
+func WithNoCache() RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		return nil, map[string]string{
+			"Cache-Control": "no-cache",
+		}, nil
+	}
+}
+
+func WithBody(body []byte) RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		return bytes.NewBuffer(body), nil, nil
+	}
+}
+
+func WithJSONBody(body any) RequestOption {
 	return func() (io.ReadWriter, map[string]string, error) {
 		buffer := new(bytes.Buffer)
 		err := json.NewEncoder(buffer).Encode(body)
@@ -393,6 +566,24 @@ func WithFormBody(body string) RequestOption {
 	}
 }
 
+func WithXMLBody(body any) RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		var buffer bytes.Buffer
+
+		err := xml.NewEncoder(&buffer).Encode(body)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		_, headers, err := WithContentTypeXMLHeader()()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return &buffer, headers, nil
+	}
+}
+
 func WithAcceptJSONHeader() RequestOption {
 	return WithAccept(applicationJSON)
 }
@@ -403,6 +594,10 @@ func WithContentTypeJSONHeader() RequestOption {
 
 func WithAcceptXMLHeader() RequestOption {
 	return WithAccept(applicationXML)
+}
+
+func WithContentTypeXMLHeader() RequestOption {
+	return WithContentType(applicationXML)
 }
 
 func WithContentTypeFormHeader() RequestOption {
@@ -425,9 +620,13 @@ func WithAccept(value string) RequestOption {
 	return WithHeader(acceptHeader, value)
 }
 
+func WithBearerToken(token string) RequestOption {
+	return WithHeader(authorizationHeader, fmt.Sprintf("Bearer %s", token))
+}
+
 func (c *BaseHttpClient) NewRequest(ctx context.Context, method string, url *url.URL, options ...RequestOption) (*http.Request, error) {
 	var buffer io.ReadWriter
-	var headers map[string]string = make(map[string]string)
+	var headers = make(map[string]string)
 	for _, option := range options {
 		buf, h, err := option()
 		if err != nil {
